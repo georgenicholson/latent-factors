cfbc283e0994eb1ed5ea4b653bdcb463995c27de
Fabian Falck <fabian.falck1@gmail.com>
2024-04-28 23:42:22 +0100

    small correction

--- scripts/p09_attach_sweep.py
+++ scripts/p09_attach_sweep.py
@@ -3,8 +3,11 @@ import argparse
 
 from p09_rnn import run
 
+entity = 'your-entity-name'
+project_name = 'your-project-name'
+
 parser = argparse.ArgumentParser(description='Attaching to a sweep.')
 parser.add_argument('--sweep_id', type=str, default="4nvmgvhv", metavar='N', help="ID of sweep to attach to.")
 H, unknown = parser.parse_known_args()
 
-wandb.agent("fabianfalck" + '/' + "jbi_second" + '/' + H.sweep_id, function=run)
+wandb.agent(entity + '/' + project_name + '/' + H.sweep_id, function=run)
